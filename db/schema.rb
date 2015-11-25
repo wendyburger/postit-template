1126aeb1004356a85232fdbd3021ad96ef46ecf5
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-11-25 15:59:00 +0800

    Create modle comment and associate 1:M

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125072908) do
+ActiveRecord::Schema.define(version: 20151125074935) do
+
+  create_table "comments", force: true do |t|
+    t.text    "body"
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: true do |t|
     t.string  "url"
