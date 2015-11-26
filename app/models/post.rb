952c344df691e05a244921a59773a031e83905f9
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-11-26 17:30:24 +0800

    Added timestamp to comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  belongs_to :creater, foreign_key: "user_id", class_name: "User"
+  belongs_to :creator, foreign_key: "user_id", class_name: "User"
   has_many :comments
 end
