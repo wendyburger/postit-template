dcc0184f323f9ea58d58b7b2c8c688806ab30e50
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-12-11 14:40:31 +0800

    Made friendly datetime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def fix_url(str)
     str.starts_with?("http://") ? str : "http://#{str}"
   end
+
+  def display_datetime(dt)
+    dt.strftime('%y/%m/%d %l:%M%P %Z')
+  end
 end
