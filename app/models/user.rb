18d79e331fc99b891a959662faafb6fdba712867
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-12-16 15:06:32 +0800

    Validation of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   has_many :comments
 
   has_secure_password validations: false
-  validates :username, :password, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, length: {minimum: 3}
 end
