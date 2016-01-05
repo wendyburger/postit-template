93d26de907f8f0a74b53a089223938ed511f64e4
Wendy Yang <wendy.y.w.yang@gmail.com>
2016-01-05 17:04:56 +0800

    Made posts and users slugs better

--- app/models/post.rb
+++ app/models/post.rb
@@ -29,6 +29,29 @@ class Post < ActiveRecord::Base
   end
 
   def generate_slug
-    self.slug = self.title.gsub(" ", "-").downcase
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_stuffix(the_slug, count)
+      post = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_stuffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,40 @@ class User < ActiveRecord::Base
   has_many :votes
 
   has_secure_password validations: false
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true
   validates :password, presence: true, on: :create, length: {minimum: 3}
 
   before_save :generate_slug
 
-  def generate_slug
-    self.slug = self.username.gsub(" ", "-").downcase
-  end
-
   def to_param
     self.slug
   end
+
+  def generate_slug
+    the_slug = to_slug(self.username)
+    user = User.find_by slug: the_slug
+    count = 2
+    while user && user != self
+      the_slug = append_stuffix(the_slug, count)
+      user = User.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_stuffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
+  end
+
 end
