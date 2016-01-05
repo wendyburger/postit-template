45c6f6e06bac4f7a4c1d00f0628d31a6927e8d74
Wendy Yang <wendy.y.w.yang@gmail.com>
2016-01-05 14:46:01 +0800

    Add users and categories slugs

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,14 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :post_categories
 
   validates :name, presence: true
+
+  before_save :generate_slug
+  
+  def generate_slug
+    self.slug = self.name.gsub(" ","-").downcase
+  end
+
+  def to_param
+    self.slug
+  end
 end
